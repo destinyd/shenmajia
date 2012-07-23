f9aad585175928a47fa6b1270b11593532a1a463
destinyd <destinyd_war@163.com>
2012-07-23 15:16:01 +0800

    good name no uniq now

---
+++ db/migrate/20120710114741_index_modify_for_good.rb
@@ -0,0 +1,13 @@
+class IndexModifyForGood < ActiveRecord::Migration
+  def change
+    remove_index :goods, :name
+    add_index :goods,:name
+    add_index :goods, :barcode, :uniq => true
+  end
+  
+  def down
+    remove_index :goods, :barcode
+    remove_index :goods, :name
+    add_index :goods,:name,:unique => true
+  end
+end
