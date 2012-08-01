fcd25b541803dc1c7323239ceb480f86f6e05534
destinyd <destinyd_war@163.com>
2012-08-01 16:09:51 +0800

    good sweeper fix

--- app/sweepers/good_sweeper.rb
+++ app/sweepers/good_sweeper.rb
@@ -3,7 +3,6 @@ class GoodSweeper < ActionController::Caching::Sweeper
 
   def after_create(good)
     expire_cache_for(good) if good.id % 10 == 0
-    expire_cache_for_belongs_to(good)
   end
  
   # If our sweeper detects that a good was updated call this
@@ -17,28 +16,8 @@ class GoodSweeper < ActionController::Caching::Sweeper
     #expire_cache_for(good)
     #expire_cache_for_single(good)
   #end
+
   private
-  def expire_cache_for_belongs_to(good)
-    expire_cache_for_other(good.locatable) unless good.locatable.blank?
-    expire_cache_for_other(good.good) unless good.good.blank?
-    #expire_cache_for_other(good.price) unless good.price.blank?
-  end
-
-  def expire_cache_for_other(obj)
-    p = "/#{obj.class.name.downcase.pluralize}/#{obj.id}"
-    expire_page(p)
-    expire_cache_for_obj_goods(p)
-  end
-
-  def expire_cache_for_obj_goods(dir)
-    p="#{Rails.root}/public#{dir}"
-    FileUtils.rm_r p if File.exist? p
-  end
-
-  #def expire_cache_for_single(good)
-    #expire_page(:controller => 'goods', :action => 'show')
-  #end
-  
   def expire_cache_for(good)
     expire_page(:controller => 'goods', :action => 'index')
     #expire_page '/goods.html'
