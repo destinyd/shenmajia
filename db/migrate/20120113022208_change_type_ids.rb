6c95236c0f8eafabba791b675d8bd1ac6550f1bf
destinyd <destinyd_war@163.com>
2012-09-26 11:45:10 +0800

    db mig fix bug

--- db/migrate/20120113022208_change_type_ids.rb
+++ db/migrate/20120113022208_change_type_ids.rb
@@ -1,11 +1,15 @@
 class ChangeTypeIds < ActiveRecord::Migration
   def up
-    Price.where(:type_id => 1).each{|price| price.update_attribute :type_id,6}
-    Price.where(:type_id => 11).each{|price| price.update_attribute :type_id,1}
+    #Price.where(:type_id => 1).each{|price| price.update_attribute :type_id,6}
+    #Price.where(:type_id => 11).each{|price| price.update_attribute :type_id,1}
+    execute "update prices set type_id=7 where type_id = 1"
+    execute "update prices set type_id=1 where type_id = 11"
   end
 
   def down
-    Price.where(:type_id => 1).each{|price| price.update_attribute :type_id,11}
-    Price.where(:type_id => 6).each{|price| price.update_attribute :type_id,1}
+    execute "update prices set type_id=11 where type_id = 1"
+    execute "update prices set type_id=1 where type_id = 6"
+    #Price.where(:type_id => 1).each{|price| price.update_attribute :type_id,11}
+    #Price.where(:type_id => 6).each{|price| price.update_attribute :type_id,1}
   end
 end
