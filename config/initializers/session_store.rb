d6ba1154df88c7ddb49371b8504bbb8e242bc985
destinyd <destinyd_war@163.com>
2012-10-31 19:45:11 +0800

    session domain all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Zhekou::Application.config.session_store :cookie_store, :key => '_zhekou_session'
+Zhekou::Application.config.session_store :cookie_store, :key => '_zhekou_session', :domain => :all
 #Zhekou::Application.config.session_store :redis_store
 
 # Use the database for sessions instead of the cookie-based default,
