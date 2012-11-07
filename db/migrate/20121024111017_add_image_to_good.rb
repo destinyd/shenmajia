e4ef63e3b65be09408877c9126c79c18891ed240
destinyd <destinyd_war@163.com>
2012-11-07 22:39:38 +0800

    bugfix

--- db/migrate/20121024111017_add_image_to_good.rb
+++ db/migrate/20121024111017_add_image_to_good.rb
@@ -1,9 +1,5 @@
 class AddImageToGood < ActiveRecord::Migration
   def change
     add_column :goods, :image, :string
-    Good.where(picture_count:1..999).each do |good|
-      good.write_uploader(:image, good.uploads.first.read_attribute(:image_file_name))
-      good.save
-    end
   end
 end
