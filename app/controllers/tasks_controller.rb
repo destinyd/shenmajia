ec93065f285d3e82ed06d15bc0b5678b516d636a
destinyd <destinyd_war@163.com>
2013-06-03 14:24:15 +0800

    remove user task

--- app/controllers/tasks_controller.rb
+++
@@ -1,8 +0,0 @@
-class TasksController < InheritedResources::Base
-  before_filter :authenticate_admin!#, only: [:index,:new,:create,:edit,:update,:destroy]
-
-  protected
-  def collection
-    @tasks ||= end_of_association_chain.paginate(page: params[:page])
-  end
-end
--- app/controllers/user_tasks_controller.rb
+++
@@ -1,8 +0,0 @@
-class UserTasksController < InheritedResources::Base
-  before_filter :authenticate_admin!#, only: [:index,:new,:create,:edit,:update,:destroy]
-
-  protected
-  def collection
-    @user_tasks ||= end_of_association_chain.recent.paginate(page: params[:page])
-  end
-end
