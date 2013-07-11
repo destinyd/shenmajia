c4e4aceb0aa8ae9e29cbc64bcc08a1cab545d35f
destinyd <destinyd_war@163.com>
2013-07-11 16:38:12 +0800

    new sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,44 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = 'http://shenmajia.com'
+
+SitemapGenerator::Sitemap.create do
+  Bill.all.each do |c|
+    add bill_path(c), lastmod: (c.updated_at || c.created_at), :priority => 0.9
+  end
+
+  Cost.all.each do |c|
+    add cost_path(c), lastmod: (c.updated_at || c.created_at), :priority => 0.8
+  end
+
+  Price.all.each do |c|
+    add price_path(c), lastmod: (c.updated_at || c.created_at), :priority => 0.8
+  end
+
+  Good.all.each do |c|
+    add good_path(c), lastmod: (c.updated_at || c.created_at), :priority => 0.7
+  end
+
+
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.all.each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+end
