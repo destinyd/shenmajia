a0e7e441f3e82a7c6146ce473ae6cb99c34901fd
destinyd <destinyd_war@163.com>
2013-07-14 14:55:26 +0800

    articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,20 @@
 class ArticlesController < InheritedResources::Base
   before_filter :authenticate_admin!, only: [:new,:create,:edit,:update,:destroy]
 
+  def show
+    show! do
+      add_crumb(I18n.t("controller.articles"), articles_path)
+      add_crumb(@article.to_s, article_path(@article))
+    end
+  end
+
   protected
   def collection
+    if parent?
+      add_crumb(I18n.t("controller.#{parent.class.name.downcase.pluralize}"), polymorphic_path(parent.class))
+      add_crumb(parent.name, polymorphic_path(parent))
+    end
+    add_crumb(I18n.t("controller.articles"), articles_path)
     @articles ||= end_of_association_chain.recent.page(params[:page])
   end
 end
