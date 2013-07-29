51e50e8a1d3d6610a3634b62c2d45aab5c7c3bc8
destinyd <destinyd_war@163.com>
2013-07-29 23:29:13 +0800

    final total and cost

--- app/assets/javascripts/bills.js
+++ app/assets/javascripts/bills.js
@@ -37,12 +37,12 @@ function clear_search(){
 
 function add_good(id,name){
   var t = Number(new Date());
-  var html = '<tr id="good_' + id + '" class="goods_selected"><td class="image"><input id="bill_bill_prices_attributes_' + t + '_image" name="bill[bill_prices_attributes][' + t + '][image]" type="file" /></td>  <td class="name"><input id="bill_bill_prices_attributes_' + t + '_name" name="bill[bill_prices_attributes][' + t + '][name]" value="' + name + '" disabled="disabled" size="30" type="text" /><input id="bill_bill_prices_attributes_' + t + '_good_id" name="bill[bill_prices_attributes][' + t + '][good_id]" type="hidden" value="' + id + '" /></td><td class="price"><input id="bill_bill_prices_attributes_' + t + '_price_value" name="bill[bill_prices_attributes][' + t + '][price_value]" size="30" type="text" /></td><td class="amount"> <input id="bill_bill_prices_attributes_' + t + '_amount" name="bill[bill_prices_attributes][' + t + '][amount]" size="30" type="text" value="1.0" /></td><td class="total"></td><td><input id="bill_bill_prices_attributes_' + t + '__destroy" name="bill[bill_prices_attributes][' + t + '][_destroy]" type="hidden" /><a href="#" class="remove_fields dynamic">X</a></td></tr>';
-  if($('#tbody good_' + id).length == 0){
+  var html = '<tr id="' + id + '" class="goods_selected"><td class="image"><input id="bill_bill_prices_attributes_' + t + '_image" name="bill[bill_prices_attributes][' + t + '][image]" type="file" /></td>  <td class="name"><input id="bill_bill_prices_attributes_' + t + '_name" name="bill[bill_prices_attributes][' + t + '][name]" value="' + name + '" disabled="disabled" size="30" type="text" /><input id="bill_bill_prices_attributes_' + t + '_good_id" name="bill[bill_prices_attributes][' + t + '][good_id]" type="hidden" value="' + id + '" /></td><td class="price"><input id="bill_bill_prices_attributes_' + t + '_price_value" name="bill[bill_prices_attributes][' + t + '][price_value]" size="30" type="text" /></td><td class="amount"> <input id="bill_bill_prices_attributes_' + t + '_amount" name="bill[bill_prices_attributes][' + t + '][amount]" size="30" type="text" value="1.0" /></td><td class="total"></td><td><input id="bill_bill_prices_attributes_' + t + '__destroy" name="bill[bill_prices_attributes][' + t + '][_destroy]" type="hidden" /><a href="#" class="remove_fields dynamic">X</a></td></tr>';
+  if($('#tbody ' + id).length == 0){
     $('#tbody').prepend(html);
     $('#good_query').val('');
     $('#results').html('');
-    $('#goodModal').modal('hide')
+    $('#modalGoodSearch').modal('hide')
   }
   else{
     alter('你已添加此商品。');
@@ -53,4 +53,45 @@ function bind_new_good (btn_selector) {
   $(btn_selector).click(function(){
     $('#modalNewGood').modal('show');
   })
+}
+
+function format_number(pnumber,decimals){
+  if (isNaN(pnumber)) { return 0};
+  if (pnumber=='') { return 0};
+
+  var snum = new String(pnumber);
+  var sec = snum.split('.');
+  var whole = parseFloat(sec[0]);
+  var result = '';
+
+  if(sec.length > 1){
+    var dec = new String(sec[1]);
+    dec = String(parseFloat(sec[1])/Math.pow(10,(dec.length - decimals)));
+    dec = String(whole + Math.round(parseFloat(dec))/Math.pow(10,decimals));
+    var dot = dec.indexOf('.');
+    if(dot == -1){
+      dec += '.';
+      dot = dec.indexOf('.');
+    }
+    while(dec.length <= dot + decimals) { dec += '0'; }
+    result = dec;
+  } else{
+    var dot;
+    var dec = new String(whole);
+    dec += '.';
+    dot = dec.indexOf('.');    
+    while(dec.length <= dot + decimals) { dec += '0'; }
+    result = dec;
+  }  
+  return result;
+}
+
+function goods_total (goods) {
+  all_total = 0.0;
+  $.each(goods,function(key,val){
+    all_total += val;
+  })
+  var str_all_total = format_number(all_total, 2);
+  $('#origin_total').html(str_all_total);
+  $('#bill_total,#bill_cost').val(str_all_total);
 }
