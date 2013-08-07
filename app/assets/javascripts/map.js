2991121d28a417ebfbe50dd594ca90dbdf596359
destinyd <destinyd_war@163.com>
2013-08-07 20:07:05 +0800

    point select in bill form

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,16 +3,13 @@ function geo_bind_selector(myGeo, selector, lat_selector, lon_selector, e ){
   $(selector).on(e, function(){
     // 将地址解析结果显示在地图上,并调整地图视野
     myGeo.getPoint($(this).val(), function(point){
-      if (point) {
-        $(lat_selector).val(point.lat);
-        $(lon_selector).val(point.lng);
-      }
+      set_bill_location(point, lat_selector, lon_selector);
     });
 
   })
 }
 
-function locate (map, geolocation, lat_selector, lon_selector) {
+function locate (map, geolocation, lat_selector, lon_selector, btn_selector, tab_selector) {
   var lat = $(lat_selector);
   var lon = $(lon_selector);
   if(lat.val()=='' || lon.val()==''){
@@ -21,6 +18,9 @@ function locate (map, geolocation, lat_selector, lon_selector) {
         lat.val(r.point.lat);
         lon.val(r.point.lng);
         map.centerAndZoom(r.point, 15);
+        map.clearOverlays();
+        set_bill_marker(map, r.point, lat_selector, lon_selector);
+        enable_bill_next_btn(btn_selector, tab_selector);
       }
     },{enableHighAccuracy: true})
   }
@@ -58,27 +58,13 @@ function suggestion_bind_selector (map, text_id, lat_selector, lon_selector, btn
   function setPlace(){
     function myFun(){
       var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
-      $(lat_selector).val(pp.lat);
-      $(lon_selector).val(pp.lng);
+      set_bill_location(pp, lat_selector, lon_selector);
       map.clearOverlays();
       map.centerAndZoom(pp, 18);
-      var myIcon = new BMap.Icon("/assets/map/marker_red_sprite.png", new BMap.Size(39,25));
-      map.addOverlay(new BMap.Marker(pp, {icon: myIcon}));
+      set_bill_marker(map, pp, lat_selector, lon_selector);
 
-      if(typeof btn_selector !== 'undefined' && typeof tab_selector !== 'undefined'){
-        var btn = $(btn_selector);
-        var tab = $(tab_selector);
-        btn.removeClass('btn-info').addClass('btn-primary');
-        btn.html('下一步');
-        tab.attr('href','#tab2');
-        tab.attr('data-toggle','tab');
-        tab.parent().removeClass('disabled');
-        btn.click(function(){
-          tab.click();
-          return false;
-        })
-        btn.attr('disabled', false);
-      }
+      enable_bill_next_btn(btn_selector, tab_selector);
+
     }
     var local = new BMap.LocalSearch(map, { //智能搜索
       onSearchComplete: myFun
@@ -86,4 +72,38 @@ function suggestion_bind_selector (map, text_id, lat_selector, lon_selector, btn
     local.search(myValue);
   }
 
+}
+
+function enable_bill_next_btn (btn_selector, tab_selector) {
+  if(typeof btn_selector !== 'undefined' && typeof tab_selector !== 'undefined'){
+    var btn = $(btn_selector);
+    var tab = $(tab_selector);
+    btn.removeClass('btn-info').addClass('btn-primary');
+    btn.html('下一步');
+    tab.attr('href','#tab2');
+    tab.attr('data-toggle','tab');
+    tab.parent().removeClass('disabled');
+    btn.click(function(){
+      tab.click();
+      return false;
+    })
+    btn.attr('disabled', false);
+  }
+}
+
+function set_bill_marker (map, point, lat_selector, lon_selector) {
+  var myIcon = new BMap.Icon("/assets/map/marker_red_sprite.png", new BMap.Size(39,25));
+  var marker = new BMap.Marker(point, {icon: myIcon, enableDragging: true});
+  marker.addEventListener("dragend", function showInfo(){
+   var cp = marker.getPosition();
+   set_bill_location(cp, lat_selector, lon_selector);
+  });
+  map.addOverlay(marker);
+}
+
+function set_bill_location (point, lat_selector, lon_selector) {
+  if(point){
+    $(lat_selector).val(point.lat);
+    $(lon_selector).val(point.lng);
+  }
 }
